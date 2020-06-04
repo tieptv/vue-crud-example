586e91919d0e573ed6364cd403acf6554cb4e746
nvtiep2 <nvtiep2@cmc.com.vn>
2020-06-04 17:22:31 +0700

    update

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,20 +1,103 @@
 <template>
 <div>
     <div class="row justify-content-center">
-       <div class="col-8 frameQuestion">
+       <div class="col-8">
        <div class="row question">
-          <span>Ai là người đẹp trai nhất xóm ?</span>
+          <span>Ai là người đẹp trai nhất xóm ? Ai là người đẹp trai nhất xóm ? Ai là người đẹp trai nhất xóm ? 
+              Ai là người đẹp trai nhất xóm ?Ai là người đẹp trai nhất xóm ?Ai là người đẹp trai nhất xóm ?
+           
+              </span>
        </div>
        <div class="row justify-content-between">
-           <div class="col-5 answer"></div>
-             <div class="col-5 answer"></div>
+           <div class="col-12 answer">
+    <b-form-checkbox
+      id="checkbox-1"
+      v-model="status"
+      name="checkbox-1"
+      value="accepted"
+      unchecked-value="not_accepted"
+    >
+      I accept the terms and use
+    </b-form-checkbox>
+           </div>
+               <div class="col-12 answer">
+    <b-form-checkbox
+      id="checkbox-1"
+      v-model="status"
+      name="checkbox-1"
+      value="accepted"
+      unchecked-value="not_accepted"
+    >
+      I accept the terms and use
+    </b-form-checkbox>
+     <b-form-checkbox
+      id="checkbox-1"
+      v-model="status"
+      name="checkbox-1"
+      value="accepted"
+      unchecked-value="not_accepted"
+    >
+      I accept the terms and use
+    </b-form-checkbox>
+     <b-form-checkbox
+      id="checkbox-1"
+      v-model="status"
+      name="checkbox-1"
+      value="accepted"
+      unchecked-value="not_accepted"
+    >
+      I accept the terms and use
+    </b-form-checkbox>
+     <b-form-checkbox
+      id="checkbox-1"
+      v-model="status"
+      name="checkbox-1"
+      value="accepted"
+      unchecked-value="not_accepted"
+    >
+      I accept the terms and use
+    </b-form-checkbox>
+     <b-form-checkbox
+      id="checkbox-1"
+      v-model="status"
+      name="checkbox-1"
+      value="accepted"
+      unchecked-value="not_accepted"
+    >
+      I accept the terms and use
+    </b-form-checkbox>
+     <b-form-checkbox
+      id="checkbox-1"
+      v-model="status"
+      name="checkbox-1"
+      value="accepted"
+      unchecked-value="not_accepted"
+    >
+      I accept the terms and use
+    </b-form-checkbox>
+           </div>
+               <div class="col-12 answer">
+    <b-form-checkbox
+      id="checkbox-1"
+      v-model="status"
+      name="checkbox-1"
+      value="accepted"
+      unchecked-value="not_accepted"
+    >
+      I accept the terms and use
+    </b-form-checkbox>
+           </div>
        </div>
        <div class="row justify-content-between">
-             <div class="col-5 answer"></div>
-             <div class="col-5 answer"></div>
+             <div class="col-12 answer"></div>
        </div>
+       
        </div>
-        <div class="col-4 frameQuestion"></div>
+        <div class="col-4 frameQuestion">
+            <div class="square">
+
+            </div>
+        </div>
     </div>
 </div>
 </template>
@@ -25,18 +108,31 @@ name: "game",
 </script>
 <style>
 .frameQuestion{
-    height: 100px;
-    border-style: groove;
+    height: 300px;
+    /* background-color: burlywood; */
+    border-radius: 20px;
 }
 .answer {
-    background-color: blue;
     height: 25px;
 }
 .question{
-    height: 50px;
+    min-height: 50px;
     font-size: 16px;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     font-weight: bold;
-;
+    background-color: burlywood;
+    border-radius: 20px;
+    padding:0px 24px 0px 24px;
+    margin-bottom: 16px;
+}
+.square {
+  height: 50px;
+  width: 50px;
+  background-color: aliceblue;
+  border-radius: 12px;
+  cursor: pointer;
+}
+.square:hover{
+    background-color: blueviolet;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
 
@@ -9,3 +12,7 @@ new Vue({router,
   store,
   render: h => h(App),
 }).$mount('#app')
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
